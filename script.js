04599dac162cf6a05e7699694f179f3352e7f1d2
Dhairya <dhairyashah.ai@gmail.com>
2025-09-21 20:26:24 -0400

    Added random color and erase btn

--- script.js
+++ script.js
@@ -3,41 +3,64 @@ const container = document.querySelector('#container');
 // initiate grid
 let squares = 16;
 createGrid(squares);
-addHoverEffect();
 
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
     squares = prompt("How many squares do you want each side?");
     
-    // clear existing grid
+    if (squares == null) {
+        squares = 16;
+    }
+
+    clearGrid();
+
+    const curSize = document.querySelector('.currentSize');
+    curSize.textContent = `Current Size: ${squares}x${squares}`;
+
+    createGrid(squares);
+})
+
+function clearGrid() {
     const toRemove = document.querySelectorAll('.grid');
     toRemove.forEach((rem) => {
         rem.remove();
     })
+}
 
-    createGrid(squares);
-    addHoverEffect();
-})
+function eraseGrid() {
+    const gridEle = document.querySelectorAll('.grid');
+    gridEle.forEach((sq) => {
+        sq.style.background = 'black';
+    })
+}
 
 function createGrid(squares) {
     for (let i=0; i<squares*squares; i++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
-        grid.style.width = `${Math.floor(1120/squares)}px`;
-        grid.style.height = `${Math.floor(1120/squares)}px`;
+        grid.style.width = `${Math.floor(500/squares)}px`;
+        grid.style.height = `${Math.floor(500/squares)}px`;
         container.appendChild(grid);
     }
+    addHoverEffect();
 }
 
 function addHoverEffect() {
     const grids = document.querySelectorAll('.grid');
     grids.forEach((grid) => {
         grid.addEventListener('mouseover', () => {
-            grid.style.background = 'rgba(98, 137, 192, 1)';
+            grid.style.background = randomColor();
         })
-        grid.addEventListener('mouseout', () => {
-            grid.style.background = 'black';
-        })
+        // grid.addEventListener('mouseout', () => {
+        //     grid.style.background = 'black';
+        // })
     })
+}
+
+function randomColor() {
+    let r = Math.floor(Math.random()*250);
+    let g = Math.floor(Math.random()*250);
+    let b = Math.floor(Math.random()*250);
+    return `rgba(${r}, ${g}, ${b})`;
 }
